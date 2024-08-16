fde2c1a9c59899c3081554704efd3ce45c676ac9
Alessandro Guida <aleguida@gmail.com>
2024-08-16 10:26:53 -0300

    add more debugging prints

--- server.py
+++ server.py
@@ -6,6 +6,9 @@ import os
 
 app = Flask(__name__)
 
+@app.route('/')
+def welcome():
+    return "Welcome to the website! The server is reachable."
 
 @app.route('/run_script', methods=['POST'])
 def run_script():
@@ -13,6 +16,12 @@ def run_script():
     gts_name = request.form.get('gts')
     propagate = request.form.get('propagate')
     checkpoint = request.form.get('checkpoint')
+
+
+    print(f"Received input_name: {input_name}")
+    print(f"Received gts_name: {gts_name}")
+    print(f"Received propagate: {propagate}")
+    print(f"Received checkpoint: {checkpoint}")
 
     script_parameters = [
         'python',
@@ -26,8 +35,16 @@ def run_script():
         '--checkpoint',
         checkpoint,
     ]
+    
+    print(f"Script parameters: {script_parameters}")
+    
     process = subprocess.Popen(script_parameters, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = process.communicate()
+
+
+    print(f"Script stdout: {stdout.decode('utf-8')}")
+    print(f"Script stderr: {stderr.decode('utf-8')}")
+    print(f"Script return code: {process.returncode}")
 
     #TODO: remove custom model?
     
@@ -39,16 +56,30 @@ def run_script():
 @app.route('/download_file', methods=['GET'])
 def download_file():
     output_name = request.form.get('output')
-    return send_file(output_name, as_attachment=True)
+    
+    print(f"Received output_name: {output_name}")
+
+    try:
+        response = send_file(output_name, as_attachment=True)
+        print(f"File {output_name} sent successfully")
+        return response
+    except Exception as e:
+        print(f"Error sending file {output_name}: {e}")
+        return str(e)
+    
+    #return send_file(output_name, as_attachment=True)
 
 @app.route('/upload', methods=['POST'])
 def upload_file():    
     file = request.files['file']
 
     if file:
+        print(f"Received file: {file.filename}")
         file.save(file.filename)
+        print(f"File saved: {file.filename}")
         return 'File uploaded successfully'
-
+    else:
+        print("No file received")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
