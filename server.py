640d4387d1b408ae3f78cea97d00bcfdef0885c7
alex1.guida <alex1.guida@nshealth.ca>
2024-08-27 22:11:47 +0000

    clean up

--- server.py
+++ server.py
@@ -88,5 +88,4 @@ def testme():
     return "Button pressed!"
 
 if __name__ == '__main__':
-    #app.run(host='0.0.0.0', port=8080, debug=True)
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
